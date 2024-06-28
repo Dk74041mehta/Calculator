4c2cd3c635958104dbc81461317fd9687f8def21
Dk74041mehta <dilipmehta70876@gmail.com>
2024-06-28 10:08:48 +0530

    first commit

---
+++ scripts.js
@@ -0,0 +1,14 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+  }
+  
+  function clearDisplay() {
+    document.getElementById('display').value = '';
+  }
+  
+  function calculate() {
+    let displayValue = document.getElementById('display').value;
+    let result = eval(displayValue); // Using eval here for simplicity (not recommended for production due to security risks)
+    document.getElementById('display').value = result;
+  }
+  
